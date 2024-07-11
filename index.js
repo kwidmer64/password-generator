1d0760bf2b7fea7523dccca1d6553a6511b75845
Korben Widmer <korbenwids@gmail.com>
2024-07-11 16:09:21 -0700

    Fixed a bug
    Fixed a bug causing the script to run before the DOM was constructed

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 let rand = getRandom();
 let pass1 = "";
 let pass2 = "";
-let generateBtn = document.getElementById("generate-btn");
+const generateBtn = document.getElementById("generate-btn");
+let genBoxOne = document.getElementById("box-one");
+let genBoxTwo = document.getElementById("box-two");
 
 
 generateBtn.addEventListener("click", function generatePasswords() {
     pass1 = "";
     pass2 = "";
-    let genBoxOne = document.getElementById("box-one");
-    let genBoxTwo = document.getElementById("box-two");
     
     for (let i = 0; i < 16; i++) {
         rand = getRandom();
